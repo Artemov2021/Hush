1f13bdac8becd46bf466efb4d8361f4d4595f89e
Tymur Artemov <Timurt005@gmail.com>
2024-07-27 14:39:02 +0200

    27.07

--- src/main/java/com/messenger/auth/SingUpController.java
+++ src/main/java/com/messenger/auth/SingUpController.java
@@ -1,35 +1,47 @@
 package com.messenger.auth;
 
 import com.messenger.design.LabelAnimation;
-import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.util.Duration;
+import javafx.scene.layout.AnchorPane;
 import javafx.event.ActionEvent;
+import javafx.stage.Stage;
 
 
 public class SingUpController {
     @FXML
+    private AnchorPane anchorPane;
+    @FXML
     private TextField emailField;
-    @FXML
-    private PasswordField passwordField;
-    @FXML
-    private Button bottomButton;
     @FXML
     private Label upperLabel;
     @FXML
+    private Label emailErrorLabel;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
     private Label lowerLabel;
+    @FXML
+    private Label passwordErrorLabel;
+    @FXML
+    private Button accountButton;
+
+    private Group passwordGroup;
 
     private String email;
     private String password;
 
     public void initialize() {
+        passwordGroup = new Group(lowerLabel, passwordField, passwordErrorLabel);
+        anchorPane.getChildren().add(passwordGroup);
+
         emailField.setFocusTraversable(false);
         passwordField.setFocusTraversable(false);
-        bottomButton.setUnderline(true);
+        accountButton.setUnderline(true);
 
         emailField.focusedProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue && emailField.getText().isEmpty()) {
@@ -37,6 +49,8 @@ public class SingUpController {
             } else if (emailField.getText().trim().isEmpty() && !newValue) {
                 emailField.setText("");
                 LabelAnimation.moveDown(upperLabel,25,2,5,25);
+            } else {
+                emailField.setText(emailField.getText().trim());
             }
         });
 
@@ -46,6 +60,11 @@ public class SingUpController {
             } else if (passwordField.getText().trim().isEmpty() && !newValue) {
                 passwordField.setText("");
                 LabelAnimation.moveDown(lowerLabel,14,4,5,22);
+            }
+        });
+        emailField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.trim().isEmpty()) {
+                emailField.setText("");
             }
         });
         passwordField.textProperty().addListener((observable, oldValue, newValue) -> {
@@ -59,6 +78,47 @@ public class SingUpController {
         email = emailField.getText().trim();
         password = passwordField.getText().trim();
 
-        System.out.println("Singed up! \nEmail: " + email + "\nPassword: " + password );
+        int emailLength = email.length();
+
+        if (!email.isEmpty() && emailLength <= 25 && !password.isEmpty()) {
+            System.out.println("Singed Up!");
+            ((Stage) anchorPane.getScene().getWindow()).close();
+        }
+
+
+        if (email.isEmpty()) {
+            emailField.getStyleClass().clear();
+            emailErrorLabel.getStyleClass().clear();
+            emailField.getStyleClass().add("input-field-error");
+            emailErrorLabel.getStyleClass().add("error-label-visible");
+            passwordGroup.setLayoutY(16);
+        } else if (email.length()>25 ) {
+            System.out.println("length: " + email.length());
+            emailField.getStyleClass().clear();
+            emailErrorLabel.getStyleClass().clear();
+            emailField.getStyleClass().add("input-field-error");
+            emailErrorLabel.getStyleClass().add("error-label-visible");
+            emailErrorLabel.setText("email or name is too long");
+            passwordGroup.setLayoutY(16);
+        } else {
+            emailField.getStyleClass().clear();
+            emailErrorLabel.getStyleClass().clear();
+            emailField.getStyleClass().add("input-field");
+            emailErrorLabel.getStyleClass().add("error-label-invisible");
+            passwordGroup.setLayoutY(0);
+        }
+
+        if (password.isEmpty()) {
+            passwordField.getStyleClass().clear();
+            passwordErrorLabel.getStyleClass().clear();
+            passwordField.getStyleClass().add("input-field-error");
+            passwordErrorLabel.getStyleClass().add("error-label-visible");
+        } else {
+            passwordField.getStyleClass().clear();
+            passwordErrorLabel.getStyleClass().clear();
+            passwordField.getStyleClass().add("input-field");
+            passwordErrorLabel.getStyleClass().add("error-label-invisible");
+        }
+
     }
 }
